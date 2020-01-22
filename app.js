84a031b6bb0f1ad8acc53ad72765b8bc9c1397bc
curro casado <currocuo9625@gmail.com>
2020-01-22 14:45:32 -0600

    creating a post

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -22,7 +23,7 @@ const postRoutes = require("./routes/post");
 
 // middleware
 app.use(morgan("dev"));
-
+app.use(bodyParser.json());
 app.use("/", postRoutes);
 
 const port = process.env.PORT || 8080;
