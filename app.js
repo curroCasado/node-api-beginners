793ba4b1357e7420c3e9a65fcd607d02981a503c
curro casado <currocuo9625@gmail.com>
2020-01-22 13:48:11 -0600

    connect to the DB using MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", err =>
+  console.log(`DB connecton error: ${err.message}`)
+);
 
 // bring in routes
 const postRoutes = require("./routes/post");
@@ -10,7 +25,7 @@ app.use(morgan("dev"));
 
 app.use("/", postRoutes);
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(8080, () => {
   console.log(`A Node Js API is listening on port: ${port}`);
 });
