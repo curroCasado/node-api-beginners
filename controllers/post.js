9330fc216b6fbca5f837580fdeba4657dac62e72
curro casado <currocuo9625@gmail.com>
2020-01-22 15:33:11 -0600

    get posts from DB

--- controllers/post.js
+++ controllers/post.js
@@ -1,9 +1,12 @@
 const Post = require("../models/post");
 
 exports.getPosts = (req, res) => {
-  res.json({
-    posts: [{ title: "First post" }, { title: "Second post" }]
-  });
+  const posts = Post.find()
+    .select("_id title body")
+    .then(posts => {
+      res.json({ posts });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.createPost = (req, res) => {
@@ -19,7 +22,7 @@ exports.createPost = (req, res) => {
   // });
 
   post.save().then(result => {
-    res.status(200).json({
+    res.json({
       post: result
     });
   });
